18831004b34a7fcad64b6862961e2d964523baf1
Riccardo Cocetta <riccardo@debian>
2015-02-25 21:59:10 +0000

    Basic initial sync + README updated

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # picloud
 Personal backup system for Raspeberry pi
+
+## What is this thing? 
+This is a little program that allows you to keep 2 discs in sync in a master slave configuration, so that you can take the master with you and always keep a safe copy on the slave connected to your Pi.
+
+My initial thoughts are detailed [here](thoughts.md)
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import json
 from settings import *
+import logging
 from os import listdir
+from datetime import datetime
+from datetime import date
+
+LOG_FORMAT = "%(asctime)-15s - [PiCloudMain][%(levelname)s] - %(message)s"
+logging.basicConfig(format=LOG_FORMAT, level=logging.DEBUG)
+logging.getLogger("picloud")
 
 def checkIs(role, path, sign='', filename='picloud.json'):
 	"""
@@ -15,15 +22,12 @@ def checkIs(role, path, sign='', filename='picloud.json'):
 		f = open(filename, 'r')
 		info = json.load(f)
 		#check that info contains a role
-		print 'info here is {0}, {1}'.format(role, info['role'])
 
 		if info['role'] == role:
-			print 'Hey'
 			#if a signature is specified, then it has to be matched
 			if (sign != '' and sign != info['signature']):
-				print "wrong sign"
+				logging.info("Found a {0} but it contains a wrong signaure".format(role))
 				return None
-			print "Found a " + role + " in " + path
 			return {'path': path, 'info': info}
 
 	except Exception as e:
@@ -56,13 +60,48 @@ def getSlave(base_path,  filename='picloud.json', sign=''):
 
 	return None;
 
+def handleSyncStart (filename):
+	now = datetime.today()
+	dateFormat = "%Y-%m-%d %H:%M:%S"
+	try:
+		f = open(filename, "r")
+	except Exception as e:
+		#No other process is syncing
+		f = open(filename, "w")
+		syncMark = {'started': now.strftime(dateFormat)}
+		print "printing syncMark"
+		print syncMark
+		json.dump(syncMark, f)
+		#We can start syncing here
+		return None
+	
+	logging.debug("Found a sync file")
+	p = json.load(f)
+	startedDate = datetime.strptime(p['started'], dateFormat)
+	
+	print startedDate
+	dateDiff = now - startedDate
+	if (dateDiff.days > 1):
+		logging.error("Looks like a process has been running for {0} days, you might want to do something about it".format(dateDiff.days))
+	if (dateDiff.days > 7):
+		#Here we might want to send an email to the admin
+		logging.error("Heyyyyy!!!! Something is really broken here. Please find out what's happening")
 
 
-master = getMaster(BASE_DIR, INFO_FILE_NAME)
-slave = None
 
-if master != None :
-	print "We have a master {0}".format(master)
-	slave = getSlave(BASE_DIR, INFO_FILE_NAME, master['info']['signature'])
+def run():
+	#check if master and slave are there
+	master = getMaster(BASE_DIR, INFO_FILE_NAME)
+	slave = None
 
-print "We have a slave {0}".format(slave)
+	if master != None :
+		logging.info("We have a master in {0}".format(master['path']))
+		slave = getSlave(BASE_DIR, INFO_FILE_NAME, master['info']['signature'])
+
+	if slave != None:
+		logging.info("We have a slave in {0}".format(slave['path']))
+
+	if master!= None and slave != None:
+		handleSyncStart("pisync.json")
+
+run()
