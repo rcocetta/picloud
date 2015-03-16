ceec28dae6f09f36d2401bcb892367bc3a716f58
Riccardo Cocetta <riccardo@debian>
2015-03-16 09:22:13 +0000

    Added CLI commands and install

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from picloudsync import PiCloudSync
 from settings import CFG
+import argparse
+import os
 
-sync = PiCloudSync(CFG)
-sync.run()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-r", "--run", help="Runs pisync once", action="store_true")
+parser.add_argument("-i", "--install", help="Installs pisync in the cronjobs, ran from the directory you're running this command from", action="store_true")
+
+args = parser.parse_args()
+
+if args.install:
+	print "Running in install mode"
+	os.system("crontab -l > cronjob")
+	os.system('echo "* * * * * cd $PWD; ptyhon main.py run; cd -" >> cronjob')
+	os.system('crontab cronjob')
+
+elif args.run: 
+	print "Running in run mode"
+	sync = PiCloudSync(CFG)
+	sync.run()
